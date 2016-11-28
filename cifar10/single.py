aec7f60a934515f7e34e12428a8ed4fa04fe22ff
Garrett Smith <g@rre.tt>
2016-11-28 14:49:01 -0600

    CIFAR10 example wip

---
+++ cifar10/single.py
@@ -0,0 +1,79 @@
+from __future__ import print_function
+
+import argparse
+
+import tensorflow as tf
+
+import support
+
+FLAGS = None
+
+def train():
+
+    images, labels = support.inputs(
+        FLAGS.data_dir,
+        support.TRAINING_DATA,
+        FLAGS.batch_size)
+
+    predictions = support.inference(images, FLAGS.batch_size)
+
+    loss = support.loss(predictions, labels)
+
+    train = support.train(loss, FLAGS.batch_size)
+
+    with training_session(loss) as session:
+        while not session.should_stop():
+            session.run(train)
+
+def training_session(loss):
+    steps = support.TRAINING_IMAGES_COUNT * FLAGS.epochs
+    hooks = [
+        tf.train.StopAtStepHook(last_step=steps),
+        tf.train.NanTensorHook(loss),
+        TrainLogHook(loss)
+    ]
+    return tf.train.MonitoredTrainingSession(
+        checkpoint_dir=FLAGS.rundir + "/train",
+        hooks=hooks,
+        config=config)
+
+class TrainLogHook(tf.train.SessionRunHook):
+
+    def __init__(self, loss):
+        super(TrainLogHook, self).__init__()
+        self.loss = loss
+
+    def begin(self):
+        self.step = -1
+
+    def before_run(self, context):
+        self.step += 1
+        return tf.train.SessionRunArgs(self.loss)
+
+    def after_run(self, context, values):
+        if self.step % 20 == 0:
+            loss = values.results
+            print("Step %i: loss=%f" % (self.step, loss)
+
+def evaluate():
+    print("TODO evaluate")
+
+def main(_):
+    support.ensure_data(FLAGS.datadir)
+    if FLAGS.prepare:
+        pass
+    elif FLAGS.evaluate:
+        evaluate()
+    else:
+        train()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datadir", default="/tmp/CIFAR10_data",)
+    parser.add_argument("--rundir", default="/tmp/CIFAR10_train")
+    parser.add_argument("--batch-size", type=int, default=128)
+    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--prepare", action="store_true")
+    parser.add_argument("--evaluate", action="store_true")
+    FLAGS, _ = parser.parse_known_args()
+    tf.app.run()
---
+++ cifar10/support.py
@@ -0,0 +1,187 @@
+from __future__ import print_function
+from __future__ import division
+
+import os
+import sys
+import tarfile
+import urllib
+
+DATA_URL = "http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz"
+DATA_BIN_NAME = "cifar-10-batches-bin"
+
+TRAINING_IMAGES_COUNT = 50000
+TEST_IMAGES_COUNT = 10000
+
+IMAGE_HEIGHT = 32
+IMAGE_WIDTH = 32
+IMAGE_DEPTH = 3
+
+CROPPED_IMAGE_HEIGHT = 24
+CROPPED_IMAGE_WIDTH = 24
+
+INPUT_LABEL_BYTES = 1
+INPUT_IMAGE_BYTES = IMAGE_HEIGHT * IMAGE_WIDTH * IMAGE_DEPTH
+INPUT_RECORD_BYTES = INPUT_LABEL_BYTES + INPUT_IMAGE_BYTES
+
+TRAINING_DATA = 1
+TEST_DATA = 2
+
+QUEUE_RUNNER_THREADS = 16
+
+###################################################################
+# Download data
+###################################################################
+
+def ensure_data(data_dir):
+    data_bin = os.path.join(data_dir, DATA_BIN_NAME)
+    if os.path.exists(data_bin):
+        return
+    data_tar = os.path.join(data_dir, DATA_URL.split("/")[-1])
+    if not os.path.exists(data_tar):
+        _download_data()
+    print("Extracting files from", data_tar)
+    tarfile.open(data_tar, "r:gz").extractall(data_dir)
+
+def _download_data(dest):
+    dest_dir = os.path.dirname(dest)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    def progress(count, block_size, total_size):
+        sys.stdout.write(
+            '\r>> Downloading %s %.1f%%'
+            % (data_tar,
+               float(count * block_size) / float(total_size) * 100.0))
+        sys.stdout.flush()
+    urllib.urlretrieve(DATA_URL, dest, progress)
+    print()
+    statinfo = os.stat(dest)
+    print("Downloaded", dest, statinfo.st_size, "bytes")
+
+###################################################################
+# Inputs
+###################################################################
+
+def inputs(data_dir, data_type, batch_size):
+
+    # Input file reader
+    filenames = _input_filenames(data_dir, data_type)
+    queue = tf.train.string_input_produce(filenames)
+    reader = tf.FixedLengthRecordReader(record_bytes=INPUT_RECORD_BYTES)
+
+    # Decode label and image
+    _key, record_raw = reader.read(queue)
+    record = tf.decode_raw(record_raw, tf.uint8)
+    label = tf.cast(tf.slice(record, [0], [INPUT_LABEL_BYTES]), tf.uint32)
+    image = tf.reshape(
+        tf.slice(record, [INPUT_LABEL_BYTES], [INPUT_IMAGE_BYTES]),
+        [IMAGE_DEPTH, IMAGE_HEIGHT, IMAGE_WIDTH])
+
+    # Transpose image from stored DHW to HWD
+    image_hwd = tf.transpose(image, [1, 2, 0])
+
+    # Resize and standardize image
+    image_float = tf.cast(image_hwd, tf.float32)
+    image_resized = tf.image.resize_image_with_crop_or_pad(
+        image_float,
+        CROPPED_IMAGE_HEIGHT,
+        CROPPED_IMAGE_WIDTH)
+    image_standardized = tf.image.per_image_standardization(image_resized)
+
+    # Process image and labels using queue runner
+    return tf.train.batch(
+        [image_standardized, label],
+        batch_size=batch_size,
+        num_threads=QUEUE_RUNNER_THREADS,
+        capacity=10 * batch_size)
+
+def _input_filenames(data_dir, data_type):
+    if data_type == TRAINING_DATA:
+        return [os.path.join(data_dir, "data_batch_%i" % i)
+                for i in range(1, 6)]
+    elif data_type == TEST_DATA:
+        return [os.path.join(data_dir, "test_batch.bin")]
+    else:
+        raise ValueError(data_type)
+
+###################################################################
+# Inference
+###################################################################
+
+def inference(images, batch_size):
+
+    # First convolutional layer
+    W_conv1 = weight_variable([5, 5, 3, 64], 0.05)
+    b_conv1 = bias_variable([64], 0.1)
+    h_conv1 = tf.nn.relu(conv2d(images, W_conv1) + b_conv1)
+    h_pool1 = max_pool_3x3(h_conv1)
+    h_norm1 = lrn(h_pool1)
+
+    # Second convolutional layer
+    W_conv2 = weight_variable([5, 5, 64, 64], 0.05)
+    b_conv2 = bias_variable([64], 0.1)
+    h_conv2 = tf.nn.relu(conv2d(h_norm1, W_conv2) + b_conv2)
+    h_norm2 = lrn(h_conv2)
+    h_pool2 = max_pool_3x3(h_norm2)
+
+    # First locally connected layer
+    h_pool2_flat = tf.reshape(h_pool2, [batch_size, -1])
+    W_local1 = weight_variable([h_pool2_flat.get_shape()[1].value, -1])
+    b_local1 = bias_variable([384], 0.1)
+    h_local1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_local1) + b_local1)
+
+    # Second locally connected layer
+    W_local2 = weight_variable([384, 192], 0.04, 0.004)
+    b_local2 = bias_variable([192])
+    h_local2 = tf.nn.relu(tf.matmul(h_local1, W_local2) + b_local2)
+
+    # Output layer
+    W_out = weight_variable([192, CLASSES_COUNT], 1/192.0)
+    b_out = bias_variable([CLASSES_COUNT], 0.0)
+    output = tf.matmul(h_local2, W_out) + b_out
+
+    return output
+
+def weight_variable(shape, stddev, decay=None):
+    W = tf.Variable(tf.truncated_normal(shape, stddev=stddev))
+    if decay is not None:
+        loss = tf.mul(tf.nn.l2_loss(W), decay)
+        tf.add_to_collection("losses", loss)
+    return W
+
+def bias_variable(shape, initial_value):
+    return tf.Variable(tf.constant(initial_value, shape))
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, [1, 1, 1, 1], padding="SAME")
+
+def max_pool_3x3(x):
+    return tf.nn.max_pool(
+        x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+
+def lrn(x):
+    return tf.nn.local_response_normalization(
+        x, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
+
+###################################################################
+# Loss
+###################################################################
+
+def loss(logits, labels):
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits, tf.cast(labels, tf.int64))
+    loss = tf.reduce_mean(cross_entropy)
+    tf.add_to_collection("losses", loss)
+    return tf.add_n(tf.get_collection("losses"))
+
+###################################################################
+# Train
+###################################################################
+
+def train(loss, batch_size):
+    global_step = tf.contrib.framework.create_global_step()
+    batches_per_epoch = TRAINING_IMAGES_COUNT / batch_size
+    lr = tf.train.exponential_decay(
+        0.1, global_step, decay_steps, 0.1, staircase=True)
+    optimizer = tf.train.GradientDescentOptimizer(lr)
+    gradients = optimizer.compute_gradients(loss)
+    return optimizer.apply_gradients(gradients, global_step=global_step)
