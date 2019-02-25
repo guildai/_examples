6674fa488b5080d0bbaaee98ed6a49659965645a
Garrett Smith <g@rre.tt>
2019-02-24 22:30:32 -0600

    Updates to mnist examples

--- mnist/intro.py
+++ mnist/intro.py
@@ -72,7 +72,7 @@ def init_variable_summaries(var, name):
 
 def init_op_summaries():
     tf.summary.scalar("loss", loss)
-    tf.summary.scalar("accuracy", accuracy)
+    tf.summary.scalar("acc", accuracy)
 
 def init_summary_writers():
     global summaries, train_writer, validate_writer
@@ -88,7 +88,7 @@ def init_collections():
     tf.add_to_collection("outputs", json.dumps({"prediction": y.name}))
     tf.add_to_collection("x", x.name)
     tf.add_to_collection("y_", y_.name)
-    tf.add_to_collection("accuracy", accuracy.name)
+    tf.add_to_collection("acc", accuracy.name)
 
 def init_session():
     global sess
@@ -141,7 +141,7 @@ def init_exported_collections():
     saver.restore(sess, FLAGS.rundir + "/model/export")
     x = tensor_by_collection_name("x")
     y_ = tensor_by_collection_name("y_")
-    accuracy = tensor_by_collection_name("accuracy")
+    accuracy = tensor_by_collection_name("acc")
 
 def tensor_by_collection_name(name):
     tensor_name = tf.get_collection(name)[0].decode("UTF-8")
