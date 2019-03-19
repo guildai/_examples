2378f85b365240a63e860ade1ad855a13314565d
Garrett Smith <g@rre.tt>
2019-03-19 12:07:35 -0500

    Simplify script

--- start/fashion_mnist_mlp.py
+++ start/fashion_mnist_mlp.py
@@ -1,12 +1,3 @@
-'''Trains a simple deep NN on the FASHION_MNIST dataset.
-
-Gets to 98.40% test accuracy after 20 epochs
-(there is *a lot* of margin for parameter tuning).
-2 seconds per epoch on a K520 GPU.
-'''
-
-from __future__ import print_function
-
 from tensorflow import keras
 
 from keras.datasets import fashion_mnist
@@ -16,49 +7,37 @@ from keras.optimizers import RMSprop
 
 batch_size = 128
 epochs = 5
+dropout = 0.2
 lr = 0.001
 lr_decay = 0.0
 rho = 0.0
 
-_num_classes = 10
-
-# the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, _num_classes)
-y_test = keras.utils.to_categorical(y_test, _num_classes)
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
 
 model = Sequential()
 model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dropout(0.2))
+model.add(Dropout(dropout))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(_num_classes, activation='softmax'))
+model.add(Dropout(dropout))
+model.add(Dense(10, activation='softmax'))
 
-model.summary()
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=RMSprop(lr=lr, rho=rho, decay=lr_decay),
+    metrics=['accuracy'])
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(lr=lr, rho=rho, decay=lr_decay),
-              metrics=['accuracy'])
-
-tensorboard = keras.callbacks.TensorBoard(".", write_images=True)
-
-history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=1,
-                    validation_data=(x_test, y_test),
-                    callbacks=[tensorboard])
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+model.fit(
+    x_train, y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    verbose=1,
+    validation_data=(x_test, y_test),
+    callbacks=[keras.callbacks.TensorBoard()])
