a3aaa1726061a1e68b5fde949e4e695c9d1d02e6
Garrett Smith <g@rre.tt>
2019-07-20 11:29:38 -0500

    Update custom scalars example

--- custom-scalars/train.py
+++ custom-scalars/train.py
@@ -3,9 +3,24 @@ import sys
 a = 1
 b = 2
 
+# Set scalar step using "step: N"
 print("step: 1")
-print("loss: 1.123")
+
+# Log scalars in the format 'KEY: VALUE'
+print("loss: 2.345")
 print("accuracy: 0.123")
 
+print("")
+
+# Setting step again applies to subsequently logged scalars
+print("step: 2")
+
+# Step 2 scalar vals
+print("loss: 1.234")
+print("accuracy: 0.456")
+
+print("")
+
 # Stderr works
+sys.stdout.flush() # Ensure previous prints are flushed
 sys.stderr.write("mse: 0.1231\n")
