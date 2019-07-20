920030f90561a91dcd7d95647713144a41e07e14
Garrett Smith <g@rre.tt>
2019-07-20 11:30:16 -0500

    Code tweak

--- custom-scalars/train.py
+++ custom-scalars/train.py
@@ -9,7 +9,6 @@ print("step: 1")
 # Log scalars in the format 'KEY: VALUE'
 print("loss: 2.345")
 print("accuracy: 0.123")
-
 print("")
 
 # Setting step again applies to subsequently logged scalars
@@ -18,7 +17,6 @@ print("step: 2")
 # Step 2 scalar vals
 print("loss: 1.234")
 print("accuracy: 0.456")
-
 print("")
 
 # Stderr works
