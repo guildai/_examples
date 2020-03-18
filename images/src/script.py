8f07ee37cbeca1e6281b1335a194ed0cee785fac
lambdaofgod <bartczukkuba@gmail.com>
2020-03-18 19:19:02 +0100

    fixed guild.yml

--- images/src/script.py
+++ images/src/script.py
@@ -2,7 +2,7 @@ import PIL
 import numpy as np
 
 
-img_path = 'data/images_train/data/123033/0e27621e3f8b484c993619abd064948be27f60a2.jpg'
-img = PIL.imread(img_path)
+img_path = 'data/123033/0e27621e3f8b484c993619abd064948be27f60a2.jpg'
+img = PIL.Image.open(img_path)
 img_array = np.asarray(img)
 print(img_array.shape)
